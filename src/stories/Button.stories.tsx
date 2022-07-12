ab3cbf270f9d1590e31a6204b05db46572564fa8
Ivan <adamovskij00@mail.ru>
2022-07-12 18:04:21 +0300

    add upd btn story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import Button from './Button'
 import { action } from '@storybook/addon-actions'
+import Button from '../components/Button'
 
 export default {
   title: 'Project/Components/Button',
   component: Button,
+  argTypes: {
+    name: {
+      title: 'Btn name',
+      description: 'Btn name',
+    },
+    status: {
+      title: 'Status value',
+      description: 'Status value',
+      defaultValue: 'ok'
+    },
+  },
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args}>Click!</Button>
 
 export const ButtonExample = Template.bind({})
 ButtonExample.args = {
+  name: 'Click!',
+  status: 'ok',
   onClick: action('Button was clicked'),
 }
