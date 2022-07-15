df2ef23b74b83560fc39ad6dcb9a969318eb3d52
Ivan <adamovskij00@mail.ru>
2022-07-15 18:34:53 +0300

    add new imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
+import ErrorBoundary from './components/ErrorBoundary'
 import { Toast, IToastPropsType } from './components/Toast'
-import ToastManager from './containers/ToastsManager'
+import ToastManager, { ContainerPositionType, ToastAnimationType, ToastPositionType } from './containers/ToastsManager'
+import { IToastListPropsType } from './components/ToastsList'
+import { ToastsListContainer } from './containers/ToastsListContainer'
 
 export { Toast, IToastPropsType }
-export { ToastManager }
+export { ToastManager, ToastPositionType, ToastAnimationType, ContainerPositionType }
+export { ErrorBoundary }
+export { IToastListPropsType }
+export { ToastsListContainer }
