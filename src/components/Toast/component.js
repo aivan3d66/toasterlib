06dbb3ffb7919ef489679f93d0b13c6ca7eca3de
Ivan <adamovskij00@mail.ru>
2022-07-15 15:56:12 +0300

    add upd Toast styles

--- src/components/Toast/component.js
+++ src/components/Toast/component.js
@@ -1,20 +1,51 @@
 import styled from 'styled-components'
+import {keyframes} from 'styled-components'
+
+const transition = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+const slideInLeft = keyframes`
+  from {
+    margin-left: 100%;
+  }
+
+  to {
+    margin-left: 0;
+  }
+`
+const slideInRight = keyframes`
+  from {
+    margin-right: 100%;
+  }
+
+  to {
+    margin-right: 0;
+  }
+`
 
 export const ToastWrapper = styled.div`
+  bottom: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 365px;
   min-height: 50px;
   max-height: 120px;
+  margin: 20px 0;
   padding: 20px 30px;
-  color: ${({ theme }) => theme.textColor};
-  background-color: ${({ theme }) => theme.backgroundColor};
+  color: ${({theme}) => theme.textColor};
+  background-color: ${({theme}) => theme.backgroundColor};
   border: 1px solid transparent;
   box-shadow: 4px 4px 8px #00000029;
   border-radius: 24px;
-  opacity: 1;
   box-sizing: border-box;
+  animation-name: ${({color}) => color === 'slideInLeft' ? slideInLeft : color === 'slideInRight' ? slideInRight : transition};
+  animation-duration: 1s;
 `
 
 export const ToastIcon = styled.div`
@@ -28,7 +59,7 @@ export const ToastIcon = styled.div`
 
 export const ToastButton = styled.button`
   margin: 0 0 auto 0;
-  color: ${({ theme }) => theme.textColor};
+  color: ${({theme}) => theme.textColor};
   font-size: 26px;
   background-color: transparent;
   border: none;
