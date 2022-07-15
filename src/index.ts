a1378e1a3a99d52f5f8acdaf66dab273d9955374
Ivan <adamovskij00@mail.ru>
2022-07-15 22:49:36 +0300

    add fix bug 0.5 and refactoring

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,16 @@
 //
 // export { iconSuccess, iconWarning, iconInfo, iconError }
 
-export * from './components';
-export * from './containers'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
+import Toast from './components/Toast/Toast'
+import ToastsList from './components/ToastsList/ToastsList'
+import ToastsListContainer from './containers/ToastsListContainer'
+import ToastsManager from './containers/ToastsManager'
+
+export default {
+  ErrorBoundary,
+  Toast,
+  ToastsList,
+  ToastsManager,
+  ToastsListContainer,
+}
