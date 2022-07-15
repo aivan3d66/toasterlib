9946208821309776a5a40ac7fe40c81e670897d9
Ivan <adamovskij00@mail.ru>
2022-07-15 15:55:39 +0300

    add upd exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { Button, IButtonPropsType } from './components/Button'
 import { Toast, IToastPropsType } from './components/Toast'
+import ToastManager from './containers/ToastsManager'
 
 export { Button, IButtonPropsType }
 export { Toast, IToastPropsType }
+export { ToastManager }
