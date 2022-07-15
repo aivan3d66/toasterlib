ab965297cf7a0c9e0ffa2f0ae8c19aee0f284d88
Ivan <adamovskij00@mail.ru>
2022-07-15 22:46:40 +0300

    add fix bug 0.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,47 @@
 import typescript from 'rollup-plugin-typescript2'
 import svgr from '@svgr/rollup'
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import url from '@rollup/plugin-url'
 import commonjs from '@rollup/plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
+import styled from 'styled-components'
 
 // import alias from '@rollup/plugin-alias';
 // import dts from "rollup-plugin-dts";
 
-import styled from 'styled-components';
-import postcss from 'postcss'
-
-const packageJson = require("./package.json");
+const packageJson = require('./package.json')
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
     external: ['styled-components'],
+    globals: { 'styled-components': 'styled' },
     plugins: [
       peerDepsExternal(),
-      // external(),
       url(),
       svgr(),
       resolve(),
       commonjs(),
       postcss(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({ tsconfig: './tsconfig.json' }),
     ],
-    globals: { 'styled-components': 'styled' },
   },
   // {
   //   input: "dist/esm/types/index.d.ts",
   //   output: [{ file: "dist/index.d.ts", format: "esm" }],
   //   plugins: [dts()],
   // },
-];
+]
