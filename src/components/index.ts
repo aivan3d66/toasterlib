41a173ee27d5bcedbe3bcae2e7456156b10a7d45
Ivan <adamovskij00@mail.ru>
2022-07-16 01:04:24 +0300

    add change types

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Toast } from './Toast'
+export { default as ToastsList } from './ToastsList'
+export { default as ErrorBoundary } from './ErrorBoundary'
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,15 @@
 // import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
-// import Toast, { IToastPropsType } from './components/Toast/Toast'
-// import { ToastManager, ContainerPositionType, ToastAnimationType, ToastPositionType } from './containers/ToastsManager'
-// import { IToastListPropsType } from './components/ToastsList/ToastsList'
+// import Toast from './components/Toast/Toast'
+// import ToastsList from './components/ToastsList/ToastsList'
 // import ToastsListContainer from './containers/ToastsListContainer'
-//
-// import { DEFAULT_TOASTS } from './constants/constants'
-//
-// import iconSuccess from '../assets/svg/iconSuccess.svg'
-// import iconWarning from '../assets/svg/iconWarning.svg'
-// import iconInfo from '../assets/svg/iconInfo.svg'
-// import iconError from '../assets/svg/iconError.svg'
+// import ToastsManager from './containers/ToastsManager'
 //
 //
-// export { Toast, IToastPropsType }
-// export { ToastManager, ToastPositionType, ToastAnimationType, ContainerPositionType }
 // export { ErrorBoundary }
-// export { IToastListPropsType }
+// export { Toast }
+// export { ToastsList }
+// export { ToastsManager }
 // export { ToastsListContainer }
-//
-// export { DEFAULT_TOASTS }
-//
-// export { iconSuccess, iconWarning, iconInfo, iconError }
 
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
-import Toast from './components/Toast/Toast'
-import ToastsList from './components/ToastsList/ToastsList'
-import ToastsListContainer from './containers/ToastsListContainer'
-import ToastsManager from './containers/ToastsManager'
-
-
-export { ErrorBoundary }
-export { Toast }
-export { ToastsList }
-export { ToastsManager }
-export { ToastsListContainer }
+export * from './components'
+export * from './containers'
