c6df408b443573432a13cde9dbc627c9c5045119
Ivan <adamovskij00@mail.ru>
2022-07-16 12:40:05 +0300

    add upd rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,10 @@
 import typescript from "@rollup/plugin-typescript";
 import svgr from '@svgr/rollup'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
-import url from '@rollup/plugin-url'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import dts from "rollup-plugin-dts";
 
-// import styled from 'styled-components'
 // import alias from '@rollup/plugin-alias';
 
 const packageJson = require('./package.json')
@@ -28,20 +25,16 @@ export default [
       },
     ],
     plugins: [
-      peerDepsExternal(),
-      url(),
       svgr(),
       resolve(),
-      commonjs(),
       postcss(),
       typescript({ tsconfig: './tsconfig.json' }),
+      commonjs(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    // external: ['styled-components'],
-    // globals: { 'styled-components': 'styled' },
   },
 ]
