5b05d51bf241e7a153b4b6d1cac0bed1a5ef3534
Ivan <adamovskij00@mail.ru>
2022-07-17 12:41:14 +0300

    add upd exports

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+export { Toast } from './components/Toast'
+export { ToastList, IToastListPropsType } from './components/ToastsList'
+export { ErrorBoundary } from './components/ErrorBoundary'
+export { ToastManager } from './containers/ToastManager'
+export { ToastListPortal } from './containers/ToastListPortal'
