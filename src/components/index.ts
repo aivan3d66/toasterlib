ed374921687fba1be64057e54b0eaa207753c826
Ivan <adamovskij00@mail.ru>
2022-07-17 12:40:21 +0300

    delete unused type files

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Toast } from './Toast'
-export { default as ToastsList } from './ToastsList'
-export { default as ErrorBoundary } from './ErrorBoundary'
--- src/containers/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ToastsManager } from './ToastManager/ToastsManager'
-export { default as ToastsListContainer } from './ToastListContainer/ToastsListContainer'
