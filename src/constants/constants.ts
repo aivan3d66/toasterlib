f55a35570ee5f006ea951a1472b12d0c10260951
Ivan <adamovskij00@mail.ru>
2022-07-18 11:22:06 +0300

    add upd imports and DEFAULT_TOASTS

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,32 +1,32 @@
-import iconSuccess from '../assets/svg/iconSuccess.svg'
-import iconWarning from '../assets/svg/iconWarning.svg'
-import iconInfo from '../assets/svg/iconInfo.svg'
-import iconError from '../assets/svg/iconError.svg'
+import IconSuccess from '@/assets/svg/iconSuccess.svg'
+import IconWarning from '@/assets/svg/iconWarning.svg'
+import IconInfo from '@/assets/svg/iconInfo.svg'
+import IconError from '@/assets/svg/iconError.svg'
 
 export const DEFAULT_TOASTS = {
   success: {
     title: 'Success',
     textColor: '#ffffff',
     backgroundColor: '#37E29A',
-    icon: iconSuccess,
+    icon: IconSuccess,
   },
   warning: {
     title: 'Warning',
     textColor: '#000000',
     backgroundColor: '#F4E048',
-    icon: iconWarning,
+    icon: IconWarning,
   },
   inform: {
     title: 'Info',
     textColor: '#ffffff',
     backgroundColor: '#9A40D3',
-    icon: iconInfo,
+    icon: IconInfo,
   },
   error: {
     title: 'Danger',
     textColor: '#ffffff',
     backgroundColor: '#E25837',
-    icon: iconError,
+    icon: IconError,
   },
 } as ToastPropertiesType
 
@@ -34,7 +34,7 @@ type DefaultToastType = {
   title: string,
   textColor: string,
   backgroundColor: string,
-  icon: string,
+  icon: any,
 }
 type ToastPropertiesType = {
   success: DefaultToastType,
