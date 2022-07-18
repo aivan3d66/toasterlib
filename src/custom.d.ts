11fe836ad239071042115c9c5d2a9c1a3f520594
Ivan <adamovskij00@mail.ru>
2022-07-18 12:59:45 +0300

    add upd svg module type

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,9 +1,6 @@
-declare module '*.svg?inline' {
-  const content: any
-  export default content
-}
-
 declare module '*.svg' {
-  const content: any
-  export default content
+  const svgUrl: string;
+  const svgComponent: SvgrComponent;
+  export default svgUrl;
+  export { svgComponent as ReactComponent };
 }
