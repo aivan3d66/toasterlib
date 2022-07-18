4d6ce2232733765c77e581a928a71521404d9ed6
Ivan <adamovskij00@mail.ru>
2022-07-18 11:08:44 +0300

    add upd types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export type ToastStatusType = 'ok' | 'warn' | 'info' | 'err'
-export type ToastPositionType = 'top-right' | 'top-left' | 'bottom-left' | 'bottom-right'
+export type ToastContainerPositionType = 'top-right' | 'top-left' | 'bottom-left' | 'bottom-right'
 export type ToastAnimationType = 'transition' | 'flip' | 'zoomIn'
-export type ContainerPositionType = {
+export type PositionValuesType = {
   top?: string,
   right?: string
   bottom?: string,
