9b8c61500ee61bf5d199439f78ffc1891a011f74
Ivan <adamovskij00@mail.ru>
2022-07-18 13:23:46 +0300

    add fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -8,5 +9,20 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "webpackFinal": async (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+    config.resolve.alias['@components'] = path.resolve(__dirname, '../src/components')
+    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config
+  }
 }
