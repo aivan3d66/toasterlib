708774c548cadb4a2d4f6501bdf2b54cf6226b68
Ivan <adamovskij00@mail.ru>
2022-07-18 10:58:57 +0300

    add useToasts

---
+++ src/hooks/useToast.ts
@@ -0,0 +1,54 @@
+import { IToastPropsType } from '@/components/Toast'
+import { ToastStatusType } from '@/types'
+import { useState } from 'react'
+// @ts-ignore
+import { v4 as uuidv4 } from 'uuid'
+
+export const useToasts = (): UseToastReturnType => {
+  const [toasts, setToasts] = useState<Array<IToastPropsType>>([])
+  const setToastId = uuidv4()
+
+  const showToast = (
+    {
+      description,
+      title,
+      status,
+      duration = 3,
+    }: ShowToastArgsType,
+  ): any => {
+    setToasts((currentState: any) => [
+      ...currentState,
+      {
+        description,
+        title,
+        status,
+        duration,
+        toastId: setToastId,
+      },
+    ])
+  }
+
+  const removeToast = (toastId: string) => {
+    setToasts((currentState) => currentState.filter((t: IToastPropsType) => t.id !== toastId))
+  }
+
+  return { toasts, showToast, removeToast }
+}
+
+type ShowToastArgsType = {
+  description?: string,
+  title?: string,
+  status: ToastStatusType,
+  duration?: number
+}
+type UseToastReturnType = {
+  toasts: Array<IToastPropsType>,
+  showToast: (
+    {
+      description,
+      title,
+      status,
+      duration,
+    }: ShowToastArgsType) => IToastPropsType,
+  removeToast: (toastId: string) => void
+}
