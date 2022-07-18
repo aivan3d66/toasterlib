abffc92e6db450aa4ac8a248bb1306d7c65d429e
Ivan <adamovskij00@mail.ru>
2022-07-18 10:58:45 +0300

    add types file

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type ToastStatusType = 'ok' | 'warn' | 'info' | 'err'
+export type ToastPositionType = 'top-right' | 'top-left' | 'bottom-left' | 'bottom-right'
+export type ToastAnimationType = 'transition' | 'flip' | 'zoomIn'
+export type ContainerPositionType = {
+  top?: string,
+  right?: string
+  bottom?: string,
+  left?: string,
+}
