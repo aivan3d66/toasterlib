42e338bac7b0aa1f3ab3fad5a99999939debea2c
Ivan <adamovskij00@mail.ru>
2022-07-18 15:18:52 +0300

    add upd ToastList story

--- src/stories/ToastListPortal.stories.tsx
+++ src/stories/ToastListPortal.stories.tsx
@@ -1,31 +1,59 @@
 import React from 'react'
 import { ToastListPortal } from '@/containers/ToastListPortal'
 import { ToastProvider, useShowToast } from '@/components/ToastProvider'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { ComponentMeta } from '@storybook/react'
 
 export default {
   title: 'Project/Components/ToastPortal',
   component: ToastListPortal,
+  argTypes: {
+    position: {
+      title: 'Container position',
+      description: 'Container position',
+      defaultValue: 'top-right'
+    },
+    animation: {
+      title: 'Toast animation',
+      description: 'Toast animation',
+    },
+    toasts: {
+      title: 'Toasts list',
+      description: 'Toast animation',
+    }
+  }
 } as ComponentMeta<typeof ToastListPortal>
 
-const Template: ComponentStory<typeof ToastListPortal> = (args) => {
-  const { showToast, toasts, removeToast } = useShowToast()
+const ToastList = () => {
+  const { showToast, toasts } = useShowToast();
+  const showToastHandler = () => {
+    showToast({
+      status: 'ok',
+      description: `Operation successful`,
+      duration: 10000
+    })
+  }
 
   return (
-    <ToastProvider>
-      <ToastListPortal position={'bottom-left'} animation={'flip'}/>
+    <>
+      <ToastListPortal position={'top-right'} animation={'flip'} toasts={toasts}/>
       <button
         type='button'
-        onClick={() => showToast({
-          status: 'ok',
-          message: `Operation successful`,
-        })}
+        onClick={showToastHandler}
       >
         add Toast
       </button>
-    </ToastProvider>
-  )
-}
-export const ToastListPortalExample = Template.bind({})
+    </>
+  );
+};
+
+export const ToastListPortalExample = () => (
+  <ToastProvider>
+      <ToastList />
+  </ToastProvider>
+);
 ToastListPortalExample.args = {
-}
+  position: 'top-right',
+  animation: 'flip'
+};
+
+
