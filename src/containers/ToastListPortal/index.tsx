c59ba45245942d9d66ba9cbb5a519e96345a8c99
Ivan <adamovskij00@mail.ru>
2022-07-18 11:03:31 +0300

    add ToastListPortal refactoring

--- src/containers/ToastListPortal/index.tsx
+++ src/containers/ToastListPortal/index.tsx
@@ -1,25 +1,51 @@
 import ReactDOM from 'react-dom'
-import React from 'react'
+import React, { useMemo } from 'react'
+import { ToastList } from '@/components/ToastsList'
+import { IToastPropsType, Toast } from '@/components/Toast'
+import { useShowToast } from '@/components/ToastProvider'
+import { usePortal } from '@/hooks/usePortal'
+import { ToastAnimationType, ToastContainerPositionType } from '@/types'
 
-const body = document.getElementById('modal-root') as HTMLElement
+export const ToastListPortal: React.FC<ToastPortalPropsType> = React.memo((
+  {
+    position,
+    animation,
+  },
+) => {
+  const { removeToast, toasts } = useShowToast()
+  const visibleToasts: Array<IToastPropsType> = useMemo(() => toasts.slice(0, 3), [toasts])
 
-export class ToastListPortal extends React.Component<{ children?: React.ReactNode }> {
-  container: HTMLElement = document.createElement('div')
+  const { isLoad, portalContainer } = usePortal()
 
-  componentDidMount() {
-    body.appendChild(this.container)
-  }
+  return isLoad
+    ? ReactDOM.createPortal(
+      <ToastList containerPosition={position}>
+        {
+          visibleToasts.map(({ status, id, description, title, duration }) => {
+              const deleteToastHandler = () => removeToast(id)
 
-  componentWillUnmount() {
-    body.removeChild(this.container)
-  }
-
-  render() {
-    return (
-      ReactDOM.createPortal(
-        this.props.children,
-        this.container,
-      )
+              return (
+                <Toast
+                  key={id}
+                  id={id}
+                  status={status}
+                  description={description}
+                  title={title}
+                  animation={animation}
+                  deleteToast={deleteToastHandler}
+                  duration={duration}
+                />
+              )
+            },
+          )
+        }
+      </ToastList>,
+      document.getElementById(portalContainer) as any,
     )
-  }
+    : null
+})
+
+type ToastPortalPropsType = {
+  position: ToastContainerPositionType,
+  animation: ToastAnimationType,
 }
