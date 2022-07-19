e3eaec228b6a1aa8e23314f421ccfa867de8be79
Ivan <adamovskij00@mail.ru>
2022-07-19 10:08:26 +0300

    add upd types

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -7,14 +7,7 @@ import { v4 as uuidv4 } from 'uuid'
 export const useToasts = () => {
   const [toasts, setToasts] = useState<Array<IToastPropsType>>([])
 
-  const showToast = (
-    {
-      description,
-      title,
-      status,
-      duration = 3,
-    }: ShowToastArgsType,
-  ): UseToastReturnType | any => {
+  const showToast = ({ description, title, status, duration }: IToastPropsType): void => {
     const setToastId: string = uuidv4()
 
     setToasts((currentState: SetStateAction<Array<IToastPropsType>>) => [
@@ -33,7 +26,7 @@ export const useToasts = () => {
     setToasts((currentState) => currentState.filter((t: IToastPropsType) => t.id !== toastId))
   }
 
-  return { toasts, showToast, removeToast }
+  return { toasts, showToast, removeToast } as UseToastReturnType
 }
 
 type ShowToastArgsType = {
@@ -42,7 +35,7 @@ type ShowToastArgsType = {
   status: ToastStatusType,
   duration?: number
 }
-type UseToastReturnType = {
+export type UseToastReturnType = {
   toasts: Array<IToastPropsType>,
   showToast: (
     {
@@ -51,5 +44,5 @@ type UseToastReturnType = {
       status,
       duration,
     }: ShowToastArgsType) => IToastPropsType,
-  removeToast: (toastId: string) => void
+  removeToast: (toastId: string) => void,
 }
